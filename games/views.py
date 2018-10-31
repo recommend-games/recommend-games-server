397978650a283ef46627bc79a6f9ed4a758c2ce7
Markus Shepherd <markus@idtmessaging.com>
2018-10-31 16:47:09 +0200

    actually sort recommendations by score; update Pipfile

--- games/views.py
+++ games/views.py
@@ -28,10 +28,10 @@ class GameViewSet(ModelViewSet):
                 from ludoj_recommender import GamesRecommender
                 self.recommender = GamesRecommender.load(
                     path=recommender_path,
-                    dir_model='.',
-                    dir_games=None,
-                    dir_ratings=None,
-                    dir_clusters=None,
+                    # dir_model='.',
+                    # dir_games=None,
+                    # dir_ratings=None,
+                    # dir_clusters=None,
                 )
             except Exception:
                 self.recommender = None
@@ -52,15 +52,24 @@ class GameViewSet(ModelViewSet):
             return Response([])
 
         # TODO handle case that user is unknown
+        # TODO speed up recommendation by pre-computing known games, clusters etc
 
         recommendation = self.recommender.recommend(
             users=(user,),
             num_games=10,
             ascending=True,
         )
+        recommendation = {game['bgg_id']: game for game in recommendation}
 
-        games = self.get_queryset().filter(bgg_id__in=recommendation['bgg_id'])
-        serializer = self.get_serializer(instance=games, many=True)
+        games = self.get_queryset().filter(bgg_id__in=recommendation)
+
+        for game in games:
+            rec = recommendation[game.bgg_id]
+            game.rec_rank = rec['rank']
+            game.rec_rating = rec['score']
+
+        serializer = self.get_serializer(
+            instance=sorted(games, key=lambda game: (game.rec_rank, -game.rec_rating)), many=True)
 
         return Response(serializer.data)
 
