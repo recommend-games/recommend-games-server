d0a232777237742ef0167b83bf6d62a86d538173
Markus Shepherd <markus@idtmessaging.com>
2018-11-05 15:33:56 +0200

    restructured static file structure; added brotli compression support

--- ludoj/settings.py
+++ ludoj/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app'),
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # WhiteNoise
