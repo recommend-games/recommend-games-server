7b39c6d33740b9042c5d764390cc859de1fa49ff
Markus Shepherd <markus@idtmessaging.com>
2018-11-19 14:57:17 +0200

    self contained release.sh

--- release.sh
+++ release.sh
@@ -1,80 +1,64 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -euxo pipefail
 
 export DEBUG=true
 export WS="${HOME}/Workspace"
 
+# before starting, make sure that
+# - results are sync'ed and merged to "${WS}/ludoj-scraper/results/"
+# - recommender models have been trained to "${WS}"/ludoj-recommender/.tc/"
+# - pipenv update --dev in "${WS}/ludoj-server"
+# - Docker is running
+
 ### SERVER ###
 cd "${WS}/ludoj-server"
-# linting
-pylint ludoj games manage.py
-cd app
-htmlhint
-htmllint
-jslint "$(find . -name '*.js')"
-jshint .
-csslint .
 # fresh database
-cd ..
-rm db.sqlite3
+mv db.sqlite3 db.sqlite3.bk || true
 python3 manage.py migrate
-python3 manage.py runserver 8000 --noreload # need to keep running
+# run server
+nohup python3 manage.py runserver 8000 --noreload >> 'server.log' 2>&1 &
+SERVER_PID="$!"
+echo "Started server with pid <${SERVER_PID}>..."
+while true && ! curl --head --fail 'http://localhost:8000/api/'; do
+    echo 'Server is not ready yet'
+    sleep 1
+done
+echo 'Server is up and running!'
 
 ### SCRAPER ###
 cd "${WS}/ludoj-scraper"
-# load latest files
-rsync --archive \
-    --rsh='ssh -p 2222' \
-    --verbose \
-    'monkeybear:~/Workspace/ludoj-scraper/feeds/' \
-    "${WS}/ludoj-scraper/feeds/"
-./merge.sh # expand
-# git add -A ; git commit -m 'updated results' ; git push
-# load data into SQLite
 python3 -m ludoj.json \
-    "${WS}/ludoj-scraper/results/bgg.csv" \
-    --output "${WS}/ludoj-scraper/feeds/bgg.jl" \
+    'results/bgg.csv' \
+    --output 'feeds/bgg.jl' \
     --url 'http://localhost:8000/api/' \
     --id-field 'bgg_id'
 python3 -m ludoj.json \
-    "${WS}/ludoj-scraper/feeds/bgg.jl" \
+    'feeds/bgg.jl' \
     --url 'http://localhost:8000/api/' \
     --id-field 'bgg_id' \
     --implementation 'implements'
 python3 -m ludoj.json \
-    "${WS}/ludoj-scraper/feeds/bgg.jl" \
+    'feeds/bgg.jl' \
     --url 'http://localhost:8000/api/' \
     --id-field 'bgg_id' \
     --fields 'designer' 'artist'
 
-### RECOMMENDER ###
-cd "${WS}/ludoj-recommender"
-rm --recursive --force '.tc'
-mkdir --parents '.tc'
-# train new recommender model
-python3 -m ludoj_recommender \
-    --train \
-    --model '.tc' \
-    --games-file "${WS}/ludoj-scraper/results/bgg.csv" \
-    --ratings-file "${WS}/ludoj-scraper/results/bgg_ratings.csv" \
-    --verbose
-# load recommender file
+### SERVER ###
+cd "${WS}/ludoj-server"
+# update recommender
+rm --recursive --force .tc static
+mkdir --parents .tc/recommender
+cp "${WS}"/ludoj-recommender/.tc/recommender/* .tc/recommender/
 python3 -m ludoj_recommender.load \
     --model '.tc' \
     --url 'http://localhost:8000/api/games/' \
     --id-field 'bgg_id' \
     --percentiles .165 .365 .615 .815 .915 .965 .985 .995
-
 # stop server now
-
+kill "${SERVER_PID}"
+sleep 10
 export DEBUG=
-
-### SERVER ###
-cd "${WS}/ludoj-server"
-rm --recursive --force .tc static
-mkdir --parents .tc/recommender
-cp "${WS}"/ludoj-recommender/.tc/recommender/* .tc/recommender/
 python3 manage.py collectstatic --no-input
 sqlite3 db.sqlite3 'VACUUM;'
 heroku container:push web
