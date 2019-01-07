9d36c470d8a8b5bc8c639232972eb7bac1c201c0
Markus Shepherd <markus@idtmessaging.com>
2019-01-07 10:51:40 +0200

    first version of NewsController

---
+++ app/js/news.js
@@ -0,0 +1,19 @@
+/*jslint browser: true, nomen: true, stupid: true, todo: true */
+/*jshint -W097 */
+/*global ludojApp, _, $ */
+
+'use strict';
+
+ludojApp.controller('NewsController', function NewsController(
+    $http,
+    $scope
+) {
+    $http.get('http://news.recommend.games/news_00000.json')
+        .then(function (response) {
+            console.log(response);
+            $scope.articles = _.get(response, 'data.results');
+        })
+        .catch(function (reason) {
+            console.log(reason);
+        });
+});
