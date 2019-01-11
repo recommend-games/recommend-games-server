a5109baf0c83d82b8607fc45ec53d668c8e251e4
Markus Shepherd <markus@idtmessaging.com>
2019-01-11 13:55:49 +0200

    standard view setup for NewsController and hover effect added to news

--- app/js/news.js
+++ app/js/news.js
@@ -6,14 +6,18 @@
 
 ludojApp.controller('NewsController', function NewsController(
     $http,
-    $scope
+    $location,
+    $scope,
+    gamesService
 ) {
     $http.get('/api/news/news_00000.json')
         .then(function (response) {
-            console.log(response);
-            $scope.articles = _.get(response, 'data.results');
-        })
-        .catch(function (reason) {
-            console.log(reason);
+            var articles = _.get(response, 'data.results');
+            $scope.articles = articles;
+            gamesService.setImage(_.get(articles, '[0].url_thumbnail[0]'));
         });
+
+    gamesService.setTitle('News aggregator');
+    gamesService.setDescription('News about board games, aggregated for you from the top sources of the hobby.');
+    gamesService.setCanonicalUrl($location.path());
 });
