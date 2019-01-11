841120d1fad6426e88333906e46049741781926d
Markus Shepherd <markus@idtmessaging.com>
2019-01-11 14:34:41 +0200

    added load more functionality to news

--- app/js/news.js
+++ app/js/news.js
@@ -8,13 +8,30 @@ ludojApp.controller('NewsController', function NewsController(
     $http,
     $location,
     $scope,
+    API_URL,
     gamesService
 ) {
-    $http.get('/api/news/news_00000.json')
-        .then(function (response) {
-            var articles = _.get(response, 'data.results');
-            $scope.articles = articles;
-            gamesService.setImage(_.get(articles, '[0].url_thumbnail[0]'));
+    function formatUrl(page) {
+        return API_URL + 'news/news_' + _.padStart(page, 5, '0') + '.json';
+    }
+
+    function fetchNews(page) {
+        page = _.parseInt(page) || 0;
+
+        return $http.get(formatUrl(page))
+            .then(function (response) {
+                var articles = _.get(response, 'data.results');
+                $scope.articles = page === 0 || _.isEmpty($scope.articles) ? articles : _.concat($scope.articles, articles);
+                $scope.nextPage = _.get(response, 'data.next');
+                $scope.total = _.get(response, 'data.count');
+            });
+    }
+
+    $scope.fetchNews = fetchNews;
+
+    fetchNews(0)
+        .then(function () {
+            gamesService.setImage(_.get($scope.articles, '[0].url_thumbnail[0]'));
         });
 
     gamesService.setTitle('News aggregator');
