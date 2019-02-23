583e32f0bd62c4a5850c8c3ab9b2ab45b266826e
Markus Shepherd <markus@idtmessaging.com>
2019-02-23 13:50:30 +0200

    added /games/stats/; changed /users/{pk}/stats/

--- games/views.py
+++ games/views.py
@@ -8,7 +8,7 @@ from functools import reduce
 from operator import or_
 
 from django.conf import settings
-from django.db.models import Q
+from django.db.models import Count, Q
 from django_filters import FilterSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
@@ -188,6 +188,19 @@ class GameViewSet(PermissionsModelViewSet):
         'owned',
     )
 
+    stats_sites = (
+        ('rg_top', 'rec_rank'),
+        ('bgg_top', 'bgg_rank'),
+    )
+
+    stats_models = (
+        ('designer', Person.objects.exclude(bgg_id=3), 'designer_of', PersonSerializer),
+        ('artist', Person.objects.exclude(bgg_id=3), 'artist_of', PersonSerializer),
+        ('game_type', GameType.objects.all(), 'games', GameTypeSerializer),
+        ('category', Category.objects.all(), 'games', CategorySerializer),
+        ('mechanic', Mechanic.objects.all(), 'games', MechanicSerializer),
+    )
+
     def _excluded_games(self, user, params):
         params = params or {}
         params.setdefault('exclude_known', True)
@@ -365,6 +378,39 @@ class GameViewSet(PermissionsModelViewSet):
             raise NotFound('unable to retrieve latest update')
         return Response({'updated_at': updated_at})
 
+    @action(detail=False)
+    def stats(self, request):
+        ''' get games stats '''
+
+        result = {'updated_at': model_updated_at()}
+
+        top_games = next(_parse_ints(request.query_params.get('top_games')), 100)
+        top_items = next(_parse_ints(request.query_params.get('top_items')), 10)
+
+        for site_key, site_rank in self.stats_sites:
+            site_filters = {f'{site_rank}__isnull': False}
+            games = frozenset(
+                self.filter_queryset(self.get_queryset())
+                .filter(**site_filters)
+                .order_by(site_rank)[:top_games]
+                .values_list('bgg_id', flat=True))
+            site_result = {'total': len(games)}
+            result[site_key] = site_result
+
+            for key, queryset, field, serializer_class in self.stats_models:
+                filters = {f'{field}__in': games}
+                objs = (
+                    queryset.annotate(top=Count(field, filter=Q(**filters)))
+                    .filter(top__gt=0)
+                    .order_by('-top')[:top_items])
+                serializer = serializer_class(
+                    objs, many=True, context=self.get_serializer_context())
+                for d, obj in zip(serializer.data, objs):
+                    d['count'] = obj.top
+                site_result[key] = serializer.data
+
+        return Response(result)
+
 
 class PersonViewSet(PermissionsModelViewSet):
     ''' person view set '''
@@ -424,6 +470,7 @@ class UserViewSet(PermissionsModelViewSet):
     serializer_class = UserSerializer
     lookup_field = 'name__iexact'
     lookup_url_kwarg = 'pk'
+    stats_sites = GameViewSet.stats_sites
 
     # pylint: disable=unused-argument,invalid-name
     @action(detail=True)
@@ -431,23 +478,28 @@ class UserViewSet(PermissionsModelViewSet):
         ''' get user stats '''
         user = self.get_object()
 
-        rg_top_100 = user.collection_set.filter(game__rec_rank__lte=100)
-        bgg_top_100 = user.collection_set.filter(game__bgg_rank__lte=100)
-
         data = {
             'user': user.name,
             'updated_at': user.updated_at,
-            'rg_top_100': {
-                'owned': rg_top_100.filter(owned=True).count(),
-                'played': rg_top_100.filter(play_count__gt=0).count(),
-                'rated': rg_top_100.filter(rating__isnull=False).count(),
-            },
-            'bgg_top_100': {
-                'owned': bgg_top_100.filter(owned=True).count(),
-                'played': bgg_top_100.filter(play_count__gt=0).count(),
-                'rated': bgg_top_100.filter(rating__isnull=False).count(),
-            },
         }
+
+        top_games = next(_parse_ints(request.query_params.get('top_games')), 100)
+
+        for key, rank in self.stats_sites:
+            games = frozenset(
+                Game.objects
+                .filter(**{f'{rank}__lte': top_games})
+                .order_by()
+                .values_list('bgg_id', flat=True))
+            filters = {f'game__in': games}
+            collection = user.collection_set.filter(**filters)
+            result = {
+                'total': len(games),
+                'owned': collection.filter(owned=True).count(),
+                'played': collection.filter(play_count__gt=0).count(),
+                'rated': collection.filter(rating__isnull=False).count(),
+            }
+            data[key] = result
 
         return Response(data)
 
