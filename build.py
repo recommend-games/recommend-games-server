852ea223d4a6bc9e53c1e0a690b8e5b31da4a391
Markus Shepherd <markus@idtmessaging.com>
2019-02-25 15:47:02 +0200

    added static commands

--- build.py
+++ build.py
@@ -8,6 +8,8 @@ import os
 import shutil
 import sys
 
+from functools import lru_cache
+
 import django
 
 from dotenv import load_dotenv
@@ -23,7 +25,7 @@ os.environ['DEBUG'] = ''
 sys.path.insert(0, BASE_DIR)
 django.setup()
 
-from games.utils import parse_date, serialize_date
+from games.utils import parse_bool, parse_date, parse_int, serialize_date
 
 LOGGER = logging.getLogger(__name__)
 SETTINGS = django.conf.settings
@@ -31,6 +33,8 @@ DATA_DIR = SETTINGS.DATA_DIR
 SCRAPER_DIR = os.path.abspath(os.path.join(BASE_DIR, '..', 'ludoj-scraper'))
 RECOMMENDER_DIR = os.path.abspath(os.path.join(BASE_DIR, '..', 'ludoj-scraper'))
 SCRAPED_DATA_DIR = os.path.abspath(os.path.join(BASE_DIR, '..', 'ludoj-data'))
+URL_LIVE = 'https://recommend.games/'
+GC_PROJECT = 'recommend-games'
 
 logging.basicConfig(
     stream=sys.stderr,
@@ -38,20 +42,24 @@ logging.basicConfig(
     format='%(asctime)s %(levelname)-8.8s [%(name)s:%(lineno)s] %(message)s',
 )
 
+# TODO: sync and merge scraped files, retrain recommender
+
+@lru_cache(maxsize=8)
+def _server_version(path=os.path.join(BASE_DIR, 'VERSION')):
+    with open(path) as file:
+        version = file.read()
+    return version.strip()
+
 
 @task()
 def cleandata(src_dir=DATA_DIR, bk_dir=f'{DATA_DIR}.bk'):
     ''' clean data file '''
-
     LOGGER.info(
         'Removing old backup dir <%s> (if any), moving current data dir to backup, '
         'and creating fresh data dir <%s>...', bk_dir, src_dir)
-
     shutil.rmtree(bk_dir, ignore_errors=True)
-
     if os.path.exists(src_dir):
         os.rename(src_dir, bk_dir)
-
     os.makedirs(os.path.join(src_dir, 'recommender'))
 
 
@@ -68,13 +76,10 @@ def filldb(
         rec_dir=os.path.join(RECOMMENDER_DIR, '.tc'),
     ):
     ''' fill database '''
-
     LOGGER.info(
         'Uploading games and other data from <%s>, and recommendations from <%s> to database...',
         src_dir, rec_dir)
-
     srp_dir = os.path.join(src_dir, 'scraped')
-
     django.core.management.call_command(
         'filldb',
         os.path.join(srp_dir, 'bgg.jl'),
@@ -131,6 +136,48 @@ def syncdata(src=os.path.join(DATA_DIR, ''), bucket='recommend-games-data'):
 @task(cleandata, filldb, compressdb, cpdirs, dateflag, syncdata)
 def releasedb():
     ''' build and release database '''
+
+
+@task()
+def cleanstatic(base_dir=BASE_DIR, sub_dirs=None):
+    ''' clean static files '''
+    sub_dirs = sub_dirs or ('.temp', 'static')
+    for sub_dir in sub_dirs:
+        target = os.path.join(base_dir, sub_dir)
+        LOGGER.info('Removing dir <%s>...', target)
+        shutil.rmtree(target, ignore_errors=True)
+
+
+@task()
+def minify(src=os.path.join(BASE_DIR, 'app'), dst=os.path.join(BASE_DIR, '.temp')):
+    ''' copy front-end files and minify HTML, JavaScript, and CSS '''
+    LOGGER.info('Copying and minifying files from <%s> to <%s>...', src, dst)
+    django.core.management.call_command('minify', src, dst, delete=True, exclude_dot=True)
+
+
+@task()
+def sitemap(url=URL_LIVE, dst=os.path.join(BASE_DIR, '.temp', 'sitemap.xml'), limit=50_000):
+    ''' generate sitemap.xml '''
+    limit = parse_int(limit) or 50_000
+    LOGGER.info('Generating sitemap with URL <%s> to <%s>, limit to %d...', url, dst, limit)
+    django.core.management.call_command('sitemap', url=url, limit=limit, output=dst)
+
+
+@task(cleanstatic, minify, sitemap)
+def collectstatic(delete=True):
+    ''' generate sitemap.xml '''
+    assert not SETTINGS.DEBUG
+
+    static_dirs = SETTINGS.STATICFILES_DIRS
+    LOGGER.info('Collecting static files from %s...', static_dirs)
+    django.core.management.call_command('collectstatic', no_input=True)
+
+    if not parse_bool(delete):
+        return
+
+    for static_dir in static_dirs:
+        LOGGER.info('Removing static dir <%s>...', static_dir)
+        shutil.rmtree(static_dir, ignore_errors=True)
 
 
 @task()
