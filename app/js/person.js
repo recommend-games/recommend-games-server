632d412342df4b14abfa340f6347aa81e2b647e8
Markus Shepherd <mk.schepke@gmail.com>
2019-09-07 16:08:04 +0300

    extended to artists

--- app/js/person.js
+++ app/js/person.js
@@ -4,15 +4,16 @@
 
 'use strict';
 
-ludojApp.controller('DesignerController', function DesignerController(
+ludojApp.controller('PersonController', function PersonController(
     $http,
     $q,
     $routeParams,
     $scope,
     API_URL
 ) {
-    var params = {
-            'role': 'designer',
+    var role = $routeParams.role || 'designer',
+        params = {
+            'role': role,
             'ordering': '-rec_rating,-bayes_rating,-avg_rating'
         },
         personPromise = $http.get(API_URL + 'persons/' + $routeParams.id + '/'),
@@ -20,7 +21,8 @@ ludojApp.controller('DesignerController', function DesignerController(
 
     $q.all([personPromise, gamesPromise])
         .then(function (results) {
-            $scope.designer = _.get(results, '[0].data');
+            $scope.person = _.get(results, '[0].data');
+            $scope.role = _.capitalize(role);
             $scope.games = _.get(results, '[1].data.results');
         });
 });
