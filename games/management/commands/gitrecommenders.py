d23d3a310a96bfc9fc3338dd161cea8bf4fe0d74
Markus Shepherd <markus.schepke@maas.global>
2019-10-16 17:33:54 +0300

    added actual training to gitrecommenders.py

--- games/management/commands/gitrecommenders.py
+++ games/management/commands/gitrecommenders.py
@@ -1,5 +1,7 @@
 import shutil
+from datetime import timezone
 from pathlib import Path
+from tempfile import TemporaryDirectory
 import turicreate as tc
 from git import Blob, Repo, Tree
 from ludoj_recommender import BGGRecommender
@@ -25,7 +27,7 @@ def _cp_files(dst, tree, game_file="bgg_GameItem.jl", rating_dir="bgg_RatingItem
         ratings_dst = dst / f"{rating_dir}.jl"
         with ratings_dst.open("wb") as ratings_fp:
             shutil.copyfileobj(ratings_blob.data_stream, ratings_fp)
-        return
+        return game_file, f"{rating_dir}.jl"
 
     ratings_tree = tree / rating_dir
     ratings_dir = dst / rating_dir
@@ -35,11 +37,29 @@ def _cp_files(dst, tree, game_file="bgg_GameItem.jl", rating_dir="bgg_RatingItem
         ratings_dst = ratings_dir / ratings_blob.name
         with ratings_dst.open("wb") as ratings_fp:
             shutil.copyfileobj(ratings_blob.data_stream, ratings_fp)
+    return game_file, rating_dir
 
 
-repo = Repo("/Users/markus/Workspace/ludoj-data-archived/")
+repo = Repo("/Users/markus/Workspace/ludoj-data-archived")
+recommender_dir = Path.home() / "recommenders-hist"
+
 for commit in repo.iter_commits(paths="scraped"):
+    date = commit.authored_datetime.astimezone(timezone.utc)
+    recommender_dst = recommender_dir / date.strftime("%Y%m%d-%H%M%S")
+    shutil.rmtree(recommender_dst, ignore_errors=True)
+    recommender_dst.mkdir(parents=True, exist_ok=True)
+    print(recommender_dst)
+
     tree = commit.tree / "scraped"
     print(tree)
-    _cp_files("/Users/markus/test-tmp", tree)
+    with TemporaryDirectory() as dst:
+        games_file, ratings_file = _cp_files(dst, tree)
+        recommender = BGGRecommender.train_from_files(
+            games_file=games_file,
+            ratings_file=ratings_file,
+            similarity_model=True,
+            max_iterations=1000,
+            verbose=True,
+        )
+        recommender.save(recommender_dst)
     break
