c402c34c193589d8c9681f0195e125b40d562954
Markus Shepherd <markus.schepke@maas.global>
2020-04-03 09:15:14 +0300

    made splitrankings command more flexible (plus bug fixes)

--- games/management/commands/splitrankings.py
+++ games/management/commands/splitrankings.py
@@ -7,12 +7,13 @@ import logging
 import os
 import sys
 
+from pathlib import Path
 from itertools import groupby
 
 import pandas as pd
 
 from django.core.management.base import BaseCommand
-from pytility import parse_date
+from pytility import arg_to_iter, clear_list, parse_date
 
 LOGGER = logging.getLogger(__name__)
 
@@ -31,10 +32,11 @@ def _process_row(row):
 
 
 def _process_file(file):
-    if isinstance(file, str):
+    if isinstance(file, (str, Path)):
         LOGGER.info("Loading rows from <%s>...", file)
         with open(file) as file_obj:
             yield from _process_file(file_obj)
+        return
 
     for row in filter(None, map(_parse_json, file)):
         yield _process_row(row)
@@ -42,22 +44,28 @@ def _process_file(file):
 
 def _process_files(files):
     for file in files:
+        file = Path(file).resolve()
         yield from _process_file(file)
 
 
-def _process_df(data_frame):
+def _process_df(data_frame, columns, target_column=None):
     if data_frame is None or data_frame.empty:
         LOGGER.error("DataFrame is empty")
         return None
 
-    columns = ["rank", "bgg_id", "bayes_rating"]
+    columns = clear_list(arg_to_iter(columns))
+    if not columns:
+        LOGGER.error("No columns given")
+        return None
+
     if any(column not in data_frame for column in columns):
         LOGGER.error("DataFrame does not contain the expected columns")
         return None
 
+    target_column = target_column or columns[0]
     return (
-        data_frame[columns][~data_frame["rank"].isna()]
-        .sort_values("rank")
+        data_frame[columns][data_frame[target_column].notna()]
+        .sort_values(target_column)
         .rename(columns={"bayes_rating": "score"})
         .astype({"rank": int, "bgg_id": int})
     )
@@ -72,7 +80,10 @@ class Command(BaseCommand):
         parser.add_argument("in_files", nargs="+")
         parser.add_argument("--out-dir", "-o", default=".")
         parser.add_argument("--out-file", "-O", default="%Y%m%d-%H%M%S.csv")
-        # parser.add_argument("--window", "-w", type=float)
+        parser.add_argument(
+            "--columns", "-c", nargs="+", default=("rank", "bgg_id", "bayes_rating")
+        )
+        parser.add_argument("--target-column", "-t")
         parser.add_argument("--overwrite", "-W", action="store_true")
         parser.add_argument("--dry-run", "-n", action="store_true")
 
@@ -85,7 +96,12 @@ class Command(BaseCommand):
 
         LOGGER.info(kwargs)
 
-        out_template = os.path.join(kwargs["out_dir"], kwargs["out_file"])
+        out_dir = Path(kwargs["out_dir"]).resolve()
+        if not kwargs["dry_run"]:
+            out_dir.mkdir(parents=True, exist_ok=True)
+            assert out_dir.is_dir()
+        LOGGER.info("Write results to dir <%s>", out_dir)
+        out_template = os.path.join(out_dir, kwargs["out_file"])
 
         for published_at, group in groupby(
             _process_files(kwargs["in_files"]), key=lambda row: row.get("published_at")
@@ -100,7 +116,11 @@ class Command(BaseCommand):
                 )
                 continue
 
-            data_frame = _process_df(pd.DataFrame.from_records(group))
+            data_frame = _process_df(
+                data_frame=pd.DataFrame.from_records(group),
+                columns=kwargs["columns"],
+                target_column=kwargs["target_column"],
+            )
 
             if data_frame is None:
                 continue
@@ -108,5 +128,4 @@ class Command(BaseCommand):
             LOGGER.info("Saving <%d> rows to <%s>...", len(data_frame), out_path)
 
             if not kwargs["dry_run"]:
-                # TODO make sure output dir exists
                 data_frame.to_csv(out_path, index=False)
