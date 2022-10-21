9ca73fa296e5f5ba78aad3a0439a420cb58eccc7
Markus Schepke <markus.schepke@wolt.com>
2022-10-21 08:48:22 +0300

    small updates taking into account deployment changes

--- release.sh
+++ release.sh
@@ -8,8 +8,8 @@ cd "$(dirname "$(readlink --canonicalize "${BASH_SOURCE[0]}")")"
 export LC_ALL=en_US.utf-8
 export LANG=en_US.utf-8
 
-docker login -u _json_key --password-stdin 'https://gcr.io' < 'gs.json'
+# docker login -u _json_key --password-stdin 'https://gcr.io' < 'gs.json'
 
-pipenv run pynt releasefull
+pipenv run pynt builddbfull
 
 cd "${SAVEDIR}"
