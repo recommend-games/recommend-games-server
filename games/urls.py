b1660a52f2324efb089649e3cd8fb04694609f83
Markus Schepke <markus.schepke@wolt.com>
2023-04-03 22:15:30 +0300

    rest_framework_proxy -> django_api_proxy

--- games/urls.py
+++ games/urls.py
@@ -2,7 +2,7 @@
 
 from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
-from rest_framework_proxy.views import ProxyView
+from django_api_proxy.views import ProxyView
 
 from .views import (
     CategoryViewSet,
@@ -32,6 +32,8 @@ urlpatterns = [
     path("redirect", redirect_view),
     path("redirect/", redirect_view),
     re_path(
-        r"^news/(?P<path>.+)$", ProxyView.as_view(source="%(path)s"), name="news-list"
+        r"^news/(?P<path>.+)$",
+        ProxyView.as_view(source="%(path)s"),
+        name="news-list",
     ),
 ]
