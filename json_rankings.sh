7de52d33880873a2e83228c49d2d463bb7b4e3d7
Markus Schepke <markus.schepke@wolt.com>
2023-04-06 10:30:00 +0300

    added json_rankings.sh

---
+++ json_rankings.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SAVE_DIR="$(pwd)"
+SERVER_DIR="$(dirname "$(readlink --canonicalize "${BASH_SOURCE[0]}")")"
+FEEDS_DIR="$(readlink --canonicalize "${SERVER_DIR}/../board-game-scraper/feeds/")"
+
+export LC_ALL=en_US.utf-8
+export LANG=en_US.utf-8
+
+cd "${SERVER_DIR}"
+
+for TYPE in 'rankings' 'abstract' 'children' 'customizable' 'family' 'party' 'strategy' 'thematic' 'war'
+do
+    [[ "${TYPE}" == 'rankings' ]] && SITE='bgg_rankings' || SITE="bgg_rankings_${TYPE}"
+    echo "Processing rankings of type <${TYPE}>…"
+    pipenv run pynt "mergebgg${TYPE}[in_paths=${FEEDS_DIR}/${SITE}/GameItem/*-json-*,days=365]" "split${TYPE}[overwrite=1]"
+done
+
+pipenv run pynt deduplicate updatecount gitupdate
+
+echo 'Done.'
+
+cd "${SAVE_DIR}"
